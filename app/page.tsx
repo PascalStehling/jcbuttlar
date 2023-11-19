01c53be682ca7125468a3743e5c34a57b8f0f41c
PascalStehling <pascal.stehling@catenion.com>
2023-11-19 15:18:36 +0000

    feat: :sparkles: add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import { Footer } from "@/components/Footer"
 import { Header } from "@/components/Header"
+import { Box } from "@mui/material"
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Box
+        sx={{
+          backgroundImage: `url("/wappen.png")`,
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          backgroundPosition: "center",
+          opacity: "75%"
+        }}
+      />
       <Footer />
     </main>
   )
